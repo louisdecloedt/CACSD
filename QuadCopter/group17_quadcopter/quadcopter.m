397b21e7d0f6233bbabe0c052de15522ff19a4f8
Jasper <j_snoeks@hotmail.be>
2020-05-07 22:26:41 +0200

    linear/discrete

---
+++ QuadCopter/group17_quadcopter/quadcopter.m
@@ -0,0 +1,44 @@
+clear all;
+close all
+
+% Parameters
+Ts = 0.05;
+m = 0.5;
+L = 0.25;
+k = 3E-6;
+b = 1E-7;
+g = 9.81;
+kd = 0.25;
+Ixx = 5E-3;
+Iyy = 5E-3;
+Izz = 1E-2;
+cm = 1E4;
+
+% initial condition
+x0 = zeros(1,12);
+
+% linear state space
+A = zeros(12,12);
+B = zeros(12,4);
+C = zeros(6,12);
+D = zeros(6,4);
+
+A(1:3,4:6) = ones(3);
+A(4:6,4:6) = -kd/m *ones(3);
+A(7:9,10:12) = ones(3);
+
+B(5,1:4) = k*cm/m;
+B(10,1) = L*k*cm/Ixx;
+B(10,3) = -B(10,1);
+B(11,2) = L*k*cm/Iyy;
+B(11,4) = -B(11,2);
+B(12,1:4) = b*cm/Izz;
+B(12,2) = -B(12,2);
+B(12,4) = -B(12,4);
+
+C(1:3,1:3) = eye(3);
+C(4:6,7:9) = eye(3);
+
+[Ad,Bd,Cd,Dd] = bilinear(A,B,C,D,1/Ts);
+
+open_system("template_quadcopter.slx")
