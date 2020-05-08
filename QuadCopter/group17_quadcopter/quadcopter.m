2bf835d066b22b5e1c1d15293278d8a97bac8769
Jasper <j_snoeks@hotmail.be>
2020-05-08 09:32:39 +0200

    matlab 2019

--- QuadCopter/group17_quadcopter/quadcopter.m
+++ QuadCopter/group17_quadcopter/quadcopter.m
@@ -39,6 +39,6 @@ B(12,4) = -B(12,4);
 C(1:3,1:3) = eye(3);
 C(4:6,7:9) = eye(3);
 
-[Ad,Bd,Cd,Dd] = bilinear(A,B,C,D,1/Ts);
+% [Ad,Bd,Cd,Dd] = bilinear(A,B,C,D,1/Ts);
 
-open_system("template_quadcopter.slx")
+open_system("template_quadcopter2019.slx")
