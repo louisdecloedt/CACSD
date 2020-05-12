5fa0d742f99b9bd36a2284adaac9611a3b1259b3
Jasper <j_snoeks@hotmail.be>
2020-05-12 17:42:00 +0200

    integral control

--- QuadCopter/group17_quadcopter/LQR_integral_control.m
+++ QuadCopter/group17_quadcopter/LQR_integral_control.m
@@ -45,36 +45,89 @@ B(12,4) = -B(12,4);
 C(1:3,1:3) = eye(3);
 C(4:6,7:9) = eye(3);
 
-% discrete transformation
-sysc = ss(A,B,C,D);
-% sysd = c2d(sysc,Ts,"zoh");
-% [A,B,C,D] = ssdata(sysd);
-
-Q = eye(18);
-R = eye(4);
-[K,~,~] = lqi(sysc,Q,R);
-K1 = K(1:4,1:6);
-K0 = K(1:4,7:18);
-
-% % solve ricatti equation
-% % Q(3,3) = 10000;
-% % Q(6,6) = 10000;
-% R = eye(size(B,2));
-% Aic = [eye(size(C,1)) C; zeros(12,6) A];
-% Bic = [D; B];
-% Q = eye(size(Bic,1));
-% [~,K,~] = idare(Aic,Bic+eye(size(Bic)),Q,R,[],[],[]);
-
-
-% sysd = c2d(sysc,Ts,"zoh");
-% [A,B,C,D] = ssdata(sysd);
-% 
+% sysc = ss(A,B,C,D);
 % Q = eye(18);
 % R = eye(4);
-% [~,K,~] = idare(A,B,Q,R,[],[],[]);
+% [K,~,~] = lqi(sysc,Q,R);
+% K0 = K(1:4,1:12);
+% K1 = K(1:4,13:18);
+
+% discrete transformation
+sysc = ss(A,B,C,D);
+sysd = c2d(sysc,Ts,"zoh");
+[A,B,C,D] = ssdata(sysd);
+
+% Q = eye(18);
+% R = eye(4);
+% [~,K,~] = idare([A zeros(12,6); C zeros(6)],[B;zeros(size(D))],Q,R);
+% size(K)
+% K0 = K(1:4,1:12);
+% K1 = K(1:4,13:18);
+
+% Aic = [eye(6) zeros(size(C)); zeros(12,6) A];
+% Bic = [zeros(size(D));B]; %-[eye(4);zeros(14,4)];
+% Q = eye(18);
+% R = eye(4);
+% [~,K,~] = idare(Aic,Bic,Q,R,[],[],[]);
+% size(K)
 % K1 = K(1:4,1:6)
 % K0 = K(1:4,7:18)
 
+% Q = eye(18);
+% R = eye(4);
+% [K,~,~] = lqi(sysc,Q,R);
+% K0 = K(1:4,1:12);
+% K1 = K(1:4,13:18);
+
+% % solve ricatti equation
+% Aic = [A zeros(12,6); -C zeros(6,6)];
+% Bic = [B;D];
+% Q = eye(18);
+% R = eye(4);
+% [~,K,~] = idare(Aic,Bic,Q,R,[],[],[]);
+% K0 = K(1:4,1:12);
+% K1 = K(1:4,13:18);
+
+
+% % solve ricatti equation
+% Aic = [A zeros(12,6); C zeros(6,6)];
+% Bic = [B;D];
+% % Cic = [C zeros(6)];
+% % Q = Cic'*Cic;
+% Q = eye(18);
+% R = eye(4);
+% [~,K,~] = idare(Aic,Bic,Q,R,[],[],[]);
+% K0 = K(1:4,1:12);
+% K1 = K(1:4,13:18);
+
+
+% Aic = [eye(size(C,1)) C; zeros(12,6) A];
+% Bic = [D; B];
+% Cic = [C zeros(6)];
+% n = size(Aic,1);
+% m = size(Bic,2);
+% p = size(C,1);
+% R = 0.1*blkdiag(eye(m),-eye(p));
+% BB = [Bic,zeros(n,p)];
+% S = [zeros(n,m),Cic'];
+% [~,K,~,info] = idare(Aic,BB,[],R,S,[],[]);
+% info
+% size(K)
+% K1 = K(1:4,1:6);
+% K0 = K(1:4,7:18);
+
+% solve ricatti equation
+Aic = [eye(size(C,1)) C; zeros(12,6) A];
+Bic = [D;B];
+Q = eye(18);
+R = eye(4);
+[~,K,~,info] = idare(Aic,Bic,Q,R);
+info
+K1 = K(1:4,1:6);
+K1 = 20*K1;
+K1(1:4,3) = 1.5*K1(1:4,3);
+K1(1:4,6) = 2*K1(1:4,6);
+K0 = 1.4*K(1:4,7:18);
 
 
 open("LQR_integral_control_quadcopter.slx");
