f39dac38119687e94169b2434241b0445658c92b
Jasper <j_snoeks@hotmail.be>
2020-05-13 14:05:07 +0200

    LQG

--- QuadCopter/group17_quadcopter/LGQ_control.m
+++ QuadCopter/group17_quadcopter/LGQ_control.m
@@ -1,6 +1,7 @@
 clear all;
 close all
 
+
 % Parameters
 Ts = 0.05;
 m = 0.5;
@@ -44,24 +45,36 @@ B(12,4) = -B(12,4);
 C(1:3,1:3) = eye(3);
 C(4:6,7:9) = eye(3);
 
-% solve ricatti equation
-Q = eye(size(C,2));
-Q(1:2,1:2) = 2*eye(2);
-Q(3,3) = 1000;
-% Q(6,6) = 10;
-% Q(7:9,7:9) = 2*eye(3);
-R = eye(size(B,2));
-[~,K,~] = icare(A,B,Q,R,[],[],[]);
-
 % discrete transformation
 sysc = ss(A,B,C,D);
 sysd = c2d(sysc,Ts,"zoh");
 [A,B,C,D] = ssdata(sysd);
 
-% determine N
-r = 6;
-N = [A-eye(size(A)) B; C D]\[zeros(18-r,r); eye(r)];
-Nx = N(1:12,:);
-Nu = N(13:16,:);
+% solve ricatti equation
+Aic = [eye(size(C,1)) C; zeros(12,6) A];
+Bic = [D;B];
+Q = eye(18);
+R = eye(4);
+[~,K,~,info] = idare(Aic,Bic,Q,R);
+K1 = K(1:4,1:6);
+K1 = 20*K1;
+K1(1:4,3) = 1.5*K1(1:4,3);
+K1(1:4,6) = 2*K1(1:4,6);
+K0 = 1.4*K(1:4,7:18);
 
-open("LQR_control_quadcopter.slx");
+% covariance matrices
+Wxyz = 2.5E-5*eye(3);
+Wptp = 7.57E-5*eye(3);
+W = eye(12);
+W(1:3,1:3) = Wxyz;
+W(7:9,7:9) = Wptp;
+V = [Wxyz zeros(3);zeros(3) Wptp];
+Qw = W*transpose(W);
+Rv = V*transpose(V);
+
+% rank(obsv(A,C))
+% [~,flag] = chol([Qw zeros(12,6);zeros(6,12) Rv])
+
+open("LQG_control_quadcopter.slx");
+sim("LQG_control_quadcopter.slx",Tmax);
+generate_report(1);
