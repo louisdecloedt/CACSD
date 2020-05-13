82f82122d9ca9f2ef0dc96b54be2eeccaf33105b
Jasper <j_snoeks@hotmail.be>
2020-05-13 19:31:27 +0200

    integrator gain

--- QuadCopter/group17_quadcopter/LQG_control.m
+++ QuadCopter/group17_quadcopter/LQG_control.m
@@ -50,33 +50,34 @@ sysc = ss(A,B,C,D);
 sysd = c2d(sysc,Ts,"zoh");
 [A,B,C,D] = ssdata(sysd);
 
-% 2 seconds without payload 2.3 with payload
 % solve ricatti equation
 Aic = [ A zeros(12,6); C eye(6,6)];
 Bic = [B;D];
 Q = 1E3*eye(18);
-R = eye(4);
+R = 1*eye(4);
 [~,K,~] = idare(Aic,Bic,Q,R,[],[],[]);
 K0 = K(1:4,1:12);
 K1 = K(1:4,13:18);
-% Igain = 16 % without payload
-Igain = 15;
+Igain = 12;
 
-% % 2.5 seconds without payload 2.8 with payload
-% % solve ricatti equation
-% Aic = [eye(size(C,1)) C; zeros(12,6) A];
-% Bic = [D;B];
-% Q = 1E11*eye(18);
-% R = 1E-11*eye(4);
-% [~,K,~,info] = idare(Aic,Bic,Q,R);
-% info
-% K1 = K(1:4,1:6)
-% K0 = K(1:4,7:18)
-% % Igain = 14 % without payload
-% Igain = 13;
+% covariance matrices
+Wxyz = 2.5E-5*eye(3);
+Wptp = 7.57E-5*eye(3);
+W = eye(12);
+W(1:3,1:3) = Wxyz;
+W(7:9,7:9) = Wptp;
+V = [Wxyz zeros(3);zeros(3) Wptp];
+Qw = W*transpose(W);
+Rv = V*transpose(V);
+% Rv = 1E9*Rv;
+% Rv(3,3) = 1E-5*Rv(3,3);
+% Rv(6,6) = 1E5*Rv(6,6);
+Rv(1:3,1:3) = 1E6*Rv(1:3,1:3);
+Rv(4:6,4:6) = 1E15*Rv(4:6,4:6);
 
+% rank(obsv(A,C))
+% [~,flag] = chol([Qw zeros(12,6);zeros(6,12) Rv])
 
-
-open("LQR_integral_control_quadcopter.slx");
-sim("LQR_integral_control_quadcopter.slx",Tmax);
-generate_report(0);
+open("LQG_control_quadcopter.slx");
+sim("LQG_control_quadcopter.slx",Tmax);
+generate_report(1);
